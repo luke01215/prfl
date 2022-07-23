e447bce04e2f7e483cd6a80f0d0deb56e8ba6e17
Luke Darling <luke01215@gmail.com>
2022-07-23 16:35:53 -0400

    Completed function to do playoff and regular season lists

--- Utility/Import-PlayerData.ps1
+++ Utility/Import-PlayerData.ps1
@@ -17,13 +17,62 @@ class Position {
     [System.Collections.Generic.List[decimal]]$seasonPointList
     [System.Collections.Generic.List[decimal]]$regularSeasonPointList
     [System.Collections.Generic.List[decimal]]$playoffSeasonPointList
+    [int]$regularSeasonEnd
+    [int]$playoffSeasonEnd
+    [int]$totalWeeksOfStats
     
+    Position([string]$rank, [string]$fullPlayerName, [decimal]$points, [decimal]$average, [string]$owner, [int]$byeweek, [int]$salary, [System.Collections.Generic.List[decimal]]$seasonPointList, [int]$regularSeasonEnd, [int]$playoffSeasonEnd, [int]$totalWeeksOfStats) {
+        [System.Collections.Generic.List[decimal]]$this.seasonPointList = New-Object -TypeName "System.Collections.Generic.List[decimal]"
+        [System.Collections.Generic.List[decimal]]$this.regularSeasonPointList = New-Object -TypeName "System.Collections.Generic.List[decimal]"
+        [System.Collections.Generic.List[decimal]]$this.playoffSeasonPointList = New-Object -TypeName "System.Collections.Generic.List[decimal]"
+        $this.rank = $rank
+        $this.fullPlayerName = $fullPlayerName
+        $this.points = $points
+        $this.average = $average
+        $this.owner = $owner
+        $this.byeweek = $byeweek
+        $this.salary = $salary
+        $this.seasonPointList = $seasonPointList
+        $this.regularSeasonEnd = $regularSeasonEnd
+        $this.playoffSeasonEnd = $playoffSeasonEnd
+        $this.totalWeeksOfStats = $totalWeeksOfStats
+        $this.CalculateRegularSeasonPointList()
+        $this.CalculatePlayoffSeasonPointList()
+    }
+
+    [void] CalculateRegularSeasonPointList() {
+        [System.Collections.Generic.List[decimal]]$list = New-Object -TypeName "System.Collections.Generic.List[decimal]"
+        for($i = 0; $i -lt $this.regularSeasonEnd; $i++) {
+            $number = $this.seasonPointList[$i]
+            $list.Add($number)
+        }
+        $this.regularSeasonPointList = $list
+    }
+
+    [void] CalculatePlayoffSeasonPointList() {
+        [System.Collections.Generic.List[decimal]]$list = New-Object -TypeName "System.Collections.Generic.List[decimal]"
+        for($i = $this.regularSeasonEnd; $i -lt $this.playoffSeasonEnd; $i++) {
+            $number = $this.seasonPointList[$i]
+            $list.Add($number)
+        }
+        $this.playoffSeasonPointList = $list
+    }
 
 }
 
 try {
+    [System.Collections.Generic.List[Position]]$positionList = New-Object -TypeName "System.Collections.Generic.List[Position]"
     $file = Import-Csv -LiteralPath $inputFileName
-    Write-Output "Test"
+    foreach($position in $file) {
+        [System.Collections.Generic.List[decimal]]$list = New-Object -TypeName "System.Collections.Generic.List[decimal]"
+        for($i = 1; $i -le $totalWeeksOfStats; $i++) {
+            $number = $position.$i
+            $list.Add($number)
+        }
+        [Position]$positionObj = [Position]::new($position.Rank, $position.Player, $position.Pts, $position.Avg, $position.Status, $position.Bye, $position.Salary, $list, $regularSeasonEnd, $playoffSeasonEnd, $totalWeeksOfStats)
+        $positionList.Add($positionObj)
+        Write-Output "Test"
+    }
 }
 catch {
     Write-Error $_.Exception.Message
